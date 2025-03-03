6dbcacf6915a4a206dec4ad210deea827ba5742b
deepvima <deepvima@usc.edu>
2025-03-03 14:41:43 -0800

    Intro Page Added

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public void Playgame(){
+        SceneManager.LoadSceneAsync("Level-1");
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
