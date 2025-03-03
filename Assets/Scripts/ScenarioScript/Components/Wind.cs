c7f87ec2c88e49ea195867ca2f2d81f6241c3441
lazy-cat-y <lazyhaoxinyang@outlook.com>
2025-03-02 21:20:59 -0800

    feat: Particle System Size

--- Assets/Scripts/ScenarioScript/Components/Wind.cs
+++ Assets/Scripts/ScenarioScript/Components/Wind.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,12 +6,23 @@ using UnityEngine;
 public class Wind : MonoBehaviour
 {
 
+    public ParticleSystem particleSystem;
+    private Vector3 windSize;
+
     public Vector2 windDirection = new Vector2(1.0f, 0.0f);
     public float windForce = 10.0f;
     // Start is called before the first frame update
     void Start()
     {
 
+        windSize = transform.childCount > 0 ? transform.GetChild(0).localScale : Vector3.one;
+
+
+        if (particleSystem == null)
+        {
+            return;
+        }
+        initParticleSystem();
     }
 
     // Update is called once per frame
@@ -18,6 +30,13 @@ public class Wind : MonoBehaviour
     {
 
     }
+
+    void LateUpdate()
+    {
+        
+    }
+
+
 
     void OnTriggerStay2D(Collider2D collision)
     {
@@ -33,4 +52,16 @@ public class Wind : MonoBehaviour
 
         collision.attachedRigidbody.AddForce(windDirection * windForce);
     }
+
+    void initParticleSystem() {
+        // shape size
+        var shape = particleSystem.shape;
+        shape.scale = new Vector3(windSize.x, 1.0f, 1.0f);
+
+        // start speed
+        var main = particleSystem.main;
+        // main.startSpeed = windForce / 3.0f;
+        main.startSpeed = (ParticleSystem.MinMaxCurve)(Math.Log(windForce) * 2.0f);
+    }
+
 }
